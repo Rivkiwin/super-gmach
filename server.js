fa9e3f842679ea7fc7129f9e6791345d56c6dffb
Rivkiwin <rivki.vinter@gmail.com>
2020-04-20 01:05:39 +0300

    server5

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
-﻿var express = require('express')
-var path = require('path')
-var serveStatic = require('serve-static')
+﻿let express = require('express'),
+    path = require('path');
+var app = express();
+var port = process.env.PORT || 8000;
+let server = require('http').Server(server);
 
-var app = express()
-app.use(express.static('./dist/super-gmach'));
-// app.use(serveStatic(path.join(super-gmach, 'dist')))
-app.get('', function(req, res) {
-  res.sendFile('index.html', {root: 'dist/super-gmach'}
-);
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', function(req, res, next){
+    res.sendStatus(200);
 });
-// var port = process.env.PORT || 8000
-// app.listen(port)
-app.listen(process.env.PORT || 8080);
-console.log('server started ' + port)
+
+app.get('/blog.html', function(req, res,next){
+    res.sendFile(path.join(__dirname+"/blog.html"));
+});
+server.listen(port, function() {
+  console.log("App is running on port " + port);
+});
+const port = process.env.PORT || 8000;
+server.listen(port, () => {
+  console.log("App is running on port " + port);
+});
