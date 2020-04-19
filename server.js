ca12fff3fca09daa745c65de960c3045f3070a05
Rivkiwin <rivki.vinter@gmail.com>
2020-04-20 00:21:39 +0300

    server.3

--- server.js
+++ server.js
@@ -3,8 +3,13 @@ var path = require('path')
 var serveStatic = require('serve-static')
 
 var app = express()
-app.use(serveStatic(path.join(__dirname, 'dist')))
-
-var port = process.env.PORT || 8000
-app.listen(port)
+pp.use(express.static('./dist/super-gmach'));
+// app.use(serveStatic(path.join(super-gmach, 'dist')))
+app.get('/*', function(req, res) {
+  res.sendFile('index.html', {root: 'dist/super-gmach/'}
+);
+});
+// var port = process.env.PORT || 8000
+// app.listen(port)
+app.listen(process.env.PORT || 8080);
 console.log('server started ' + port)
