becca4f470fef571700a074e9a5a008324d93e5e
Rivkiwin <rivki.vinter@gmail.com>
2020-04-20 00:22:30 +0300

    app server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var path = require('path')
 var serveStatic = require('serve-static')
 
 var app = express()
-pp.use(express.static('./dist/super-gmach'));
+app.use(express.static('./dist/super-gmach'));
 // app.use(serveStatic(path.join(super-gmach, 'dist')))
 app.get('/*', function(req, res) {
   res.sendFile('index.html', {root: 'dist/super-gmach/'}
