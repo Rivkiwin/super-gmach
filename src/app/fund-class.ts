0f32991f912959eab87dde4a9c0069f9824a88ea
Rivkiwin <rivki.vinter@gmail.com>
2020-04-23 09:40:10 +0300

    dist

--- src/app/fund-class.ts
+++ src/app/fund-class.ts
@@ -2,12 +2,16 @@ export class FundClass {
     static cnt:number;
     name:string;
     id:number;
-    constructor(name:string)
+    Required_month:number;
+    Required_vip:boolean;
+    num_friends:number;
+    constructor(name:string,Required_month?:number,Required_vip?:boolean)
     {
         FundClass.cnt=FundClass.cnt++;
           this.name=name;
           this.id=1;
-         
+          this.Required_month=Required_month?Required_month:0;
+          this.Required_vip=Required_vip?Required_vip:false;      
     }
     
 }
