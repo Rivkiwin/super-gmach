dc1b29bc84231bcbc41a23246b6d142c7c5c93ec
Rivkiwin <rivki.vinter@gmail.com>
2020-06-08 07:09:31 +0300

    change tree

---
+++ src/app/componentes/funds/fund-list/fund-list.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { FundServiceService } from 'src/app/services/fund-service.service';
+import { FundClass, StatusE } from 'src/app/classes/fund-class';
+
+
+
+
+@Component({
+  selector: 'app-fund-list',
+  templateUrl: './fund-list.component.html',
+  styleUrls: ['./fund-list.component.scss','../../../ ']
+})
+export class FundListComponent implements OnInit {
+  public fund_list:FundClass[]=[];
+  statusE=StatusE;
+  name:string;
+  status:string;
+  key=Object.values;
+  public keyStatusFund():Array<string>
+  {
+   var keys = Object.keys(this.statusE);
+   return keys.slice(keys.length / 2);
+  }
+  constructor(private fundServis:FundServiceService,public router:Router,private activeRouter:ActivatedRoute) {
+    this.fund_list=this.fundServis.get_all();
+   }
+ 
+
+  ngOnInit(): void {
+  }
+
+}
